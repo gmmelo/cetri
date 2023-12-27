98e02c43167546f144637ae89be89db112944026
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2023-12-27 01:54:43 -0700

    descriptors

--- cetri.c
+++ cetri.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
-/* This is the bulk game code. In the future, if the project is pursued, the plan is to modularize aspects and create updated versions. */
-
 // Defining the ASCII characters used to fill CLI output.
 #define FILLED_SPACE 35   // #
 #define EMPTY_SPACE 58    // :
+
+// Characters defining spaced output
+#define SQUARE_LEFT 133   // [
+#define SQUARE_RIGHT 135  // ]
+
+// Global constants
 #define GRID_WIDTH 12
 #define GRID_HEIGHT 14
 
+
+/* This is the bulk game code. In the future, if the project is pursued, the plan is to modularize aspects and create updated versions. */
 int main()
 {
     char grid[GRID_HEIGHT][GRID_WIDTH + 1];
 
+    // Clear the grid
     for (int y = 0; y < GRID_HEIGHT; ++y) {
         for(int x = 0; x < GRID_WIDTH; ++x) {
             if (x < GRID_WIDTH - 1)
                 grid[x][y] = EMPTY_SPACE;
             else
                 grid[x][y] = '\n';
-        putchar(grid[x][y]);
         }
     }
+
+    // Display the double spaced output
+
 }
