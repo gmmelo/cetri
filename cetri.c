6bc0af877f735efb9884db876354c166d0ea0607
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2023-12-26 20:52:06 -0700

    simple grid display

--- cetri.c
+++ cetri.c
@@ -1,19 +1,24 @@
-/* This is the bulk game code. In the future, if the project is pursued, the plan is to modularize aspects and create updated versions. */
-
 #include <stdio.h>
+
+/* This is the bulk game code. In the future, if the project is pursued, the plan is to modularize aspects and create updated versions. */
 
 // Defining the ASCII characters used to fill CLI output.
 #define FILLED_SPACE 35   // #
 #define EMPTY_SPACE 58    // :
+#define GRID_WIDTH 12
+#define GRID_HEIGHT 14
 
 int main()
 {
-    for(int i = 0; i <= 10; i++) {
-        if(i < 5) {
-            putchar(FILLED_SPACE);
-        }
-        else {
-            putchar(EMPTY_SPACE);
+    char grid[GRID_HEIGHT][GRID_WIDTH + 1];
+
+    for (int y = 0; y < GRID_HEIGHT; ++y) {
+        for(int x = 0; x < GRID_WIDTH; ++x) {
+            if (x < GRID_WIDTH - 1)
+                grid[x][y] = EMPTY_SPACE;
+            else
+                grid[x][y] = '\n';
+        putchar(grid[x][y]);
         }
     }
 }
