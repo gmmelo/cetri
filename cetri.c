c13ae52631cc072a3643480234645bcaf8947163
Gael Melo <104900699+gmmelo@users.noreply.github.com>
2024-01-23 21:59:55 -0700

    break code into functions and add pointers

--- cetri.c
+++ cetri.c
@@ -12,6 +12,8 @@
 #define GRID_WIDTH 12
 #define GRID_HEIGHT 14
 
+void doubleDraw(char (*grid)[GRID_HEIGHT][GRID_WIDTH + 1]);
+void drawChatAt(char *symbol);
 
 /* This is the bulk game code. In the future, if the project is pursued, the plan is to modularize aspects and create updated versions. */
 int main()
@@ -31,20 +33,31 @@ int main()
     // Set some example blocks as filled
     // TODO: REMOVE
     grid[4][3] = grid[5][3] = grid[5][4] = grid[6][4] = FILLED_SPACE;
+    doubleDraw(&grid);
+}
 
+void doubleDraw(char (*grid)[GRID_HEIGHT][GRID_WIDTH + 1]) {
     // Display the double spaced output
     for (int y = 0; y < GRID_HEIGHT; ++y)
-        for (int x = 0; x <= GRID_WIDTH; ++x)
-            switch (grid[y][x]) {
-                case EMPTY_SPACE:
-                    putchar(EMPTY_SPACE);
-                    putchar(EMPTY_SPACE);
-                    break;
-                case FILLED_SPACE:
-                    putchar(SQUARE_LEFT);
-                    putchar(SQUARE_RIGHT);
-                    break;
-                default:
-                    putchar('\n');
-            }
+        for (int x = 0; x <= GRID_WIDTH; ++x) {
+            char currentChar = (*grid)[y][x];
+            drawChatAt(&currentChar); 
+        }
+}
+
+void drawChatAt(char *symbolPtr) {
+    char symbol = *symbolPtr;
+    switch (symbol)
+    {
+        case EMPTY_SPACE:
+            putchar(EMPTY_SPACE);
+            putchar(EMPTY_SPACE);
+            break;
+        case FILLED_SPACE:
+            putchar(SQUARE_LEFT);
+            putchar(SQUARE_RIGHT);
+            break;
+        default:
+            putchar('\n');
+    }
 }
